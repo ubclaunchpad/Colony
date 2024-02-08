7800210038115dbc7489d916e9e54a5c40693305
Armin Talaie <talaiearmin78@gmail.com>
2024-02-07 19:19:15 -0800

    quickfix

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = new Koa();
 const router = new Router();
-const PORT = process.env.EVENT_API_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 console.log("event api port:", PORT);
 
